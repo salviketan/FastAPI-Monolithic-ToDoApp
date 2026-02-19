a103ab1e970434c97d00d9cb3612fe0d6c445dbf
salviketan <ketan.salvi777@gmail.com>
2026-02-19 23:28:40 +0530

    model struct

--- app/models.py
+++ app/models.py
@@ -1,19 +1,28 @@
-from sqlalchemy import (
-    Boolean,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    Text,
-)
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, Integer, String, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base_class import Base
 
-
-class User(Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+# class User(Base):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+#     username: Mapped[str] = mapped_column(String(25), unique=True, index=True, nullable=False)
+#     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+#     password: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
 class Task(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(), index=True, nullable=False)
+    completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
