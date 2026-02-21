fd46c1c98ba95e5a97c2dd38ffce347d7d30bf5d
salviketan <ketan.salvi777@gmail.com>
2026-02-21 23:33:21 +0530

    session & import chng

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,14 +1,13 @@
 from collections.abc import Generator
 
-from sqlalchemy.orm.session import Session
+# from app import crud, models, schemas
+from db.session import SessionLocal
 
 # from fastapi import Depends, HTTPException, status
 # from fastapi.security import OAuth2PasswordBearer
 # from jose import jwt
 # from pydantic import ValidationError
-# from sqlalchemy.orm import Session
-# from app import crud, models, schemas
-from app.db.session import SessionLocal
+from sqlalchemy.orm import Session
 
 
 def get_db() -> Generator:
