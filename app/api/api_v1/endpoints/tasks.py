25744b805f62e328ea5906ad0fdf3b5d085f758f
salviketan <ketan.salvi777@gmail.com>
2026-02-21 23:07:50 +0530

    tasks get api

--- app/api/api_v1/endpoints/tasks.py
+++ app/api/api_v1/endpoints/tasks.py
@@ -1,6 +1,25 @@
-from typing import Any
+from typing import Annotated
 
+import crud
+import models
+import schemas
+from api import deps
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
-from app import crud, schemas
-from app.api import deps
+router = APIRouter()
+
+
+@router.get("", response_model=list[schemas.Task])
+def get_tasks(
+    db: Annotated[Session, Depends(deps.get_db)],
+    skip: int = 0,
+    limit: int = 100,
+) -> list[models.Task]:
+    tasks: list[models.Task] = crud.task.get_multi(
+        db,
+        skip=skip,
+        limit=limit,
+    )
+
+    return tasks
