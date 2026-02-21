7d62e13d0719989a1c930786beef96d0f1f5d594
salviketan <ketan.salvi777@gmail.com>
2026-02-21 23:10:36 +0530

    uvicorn run path chng

--- app/run.py
+++ app/run.py
@@ -3,8 +3,8 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",  # noqa: S104
-        port=80,
+        host="localhost",  # noqa: S104
+        port=8000,
         reload=True,
-        reload_excludes="versions/*.*",
+        reload_excludes=[".venv", "versions/*.*"],
     )
