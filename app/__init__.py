3eb40eb2bf141e77dbcc5fbacd2a79dce21881a4
salviketan <ketan.salvi777@gmail.com>
2026-02-21 23:56:14 +0530

    imports for direct use

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,7 @@
-import sys
-
-from models import Task
-from psycopg2._psycopg import connection
+from crud import task  # noqa: F401
 from schemas import (
-    Task,
-    TaskCreate,
-    TaskInDB,
-    TaskUpdate,
+    Task,  # noqa: F401
+    TaskCreate,  # noqa: F401
+    TaskInDB,  # noqa: F401
+    TaskUpdate,  # noqa: F401
 )
-
-from app.api.deps import db_session
-from app.crud import task
-from app.db.init_db import init_db
-from app.prestart import (
-    db_connection,
-    db_creation,
-    run_migrations,
-    user_creation,
-)
-
-try:
-    con: connection = db_connection()
-    db, db_exists = db_creation(con)
-    miles_con: connection = db_connection(dbname=True)
-    user_creation(con=miles_con, db_name=db)
-    print("Running migrations...")
-    run_migrations()
-    print("Application pre-startup setup complete.")
-
-    init_db(db=db_session)
-except Exception as e:
-    con.rollback()
-    miles_con.rollback()
-    db_session.rollback()
-    exception_template: str = f"Traceback {type(e).__name__}: {e}, File {__file__}, Error on line {sys.exc_info()[-1].tb_lineno}\n"
-    print(exception_template)
-    raise
-finally:
-    con.close()
-    miles_con.close()
-    db_session.close()
