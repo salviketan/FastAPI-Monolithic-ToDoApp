2ed11896343d6d4e817981a54edade11294cfdd6
salviketan <ketan.salvi777@gmail.com>
2026-02-27 00:04:45 +0530

    baseCrud chng 2 sqlalchemy version

--- app/core/base_crud.py
+++ app/core/base_crud.py
@@ -39,10 +39,16 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def get_by_kwargs(
         self,
         db: Session,
-        kwargs,
         options: list | None = None,
+        **kwargs,
     ) -> list[ModelType] | None:
-        query: Select[tuple[ModelType]] = select(self.model).where(**kwargs)
+        query: Select[tuple[ModelType]] = select(self.model)
+
+        filters: list = [
+            getattr(self.model, key) == value for key, value in kwargs.items()
+        ]
+
+        query = query.where(*filters)
 
         if options:
             query = query.options(*options)
