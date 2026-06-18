a13cc77192848516d8aae8d78c1509faa185584c
salviketan <ketan.salvi777@gmail.com>
2026-06-18 12:35:37 +0530

    1st tasks test

--- backend/app/tests/test_tasks.py
+++ backend/app/tests/test_tasks.py
@@ -10,5 +10,5 @@ def test_get_tasks(mock_db_session) -> None:
     response: Response = client.get("api/v1/tasks")
 
     assert response.status_code == 200
-    # data  = response.json()
-    # assert data["name"] = ""
+    # data = response.json()
+    # assert data["name"] == ""
