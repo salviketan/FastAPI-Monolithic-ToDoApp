a6d11d698a5df64c2b9bdb9b5a106799654bcb65
salviketan <ketan.salvi777@gmail.com>
2026-06-20 21:01:20 +0530

    code 4 only unit test

--- backend/app/tests/test_tasks.py
+++ backend/app/tests/test_tasks.py
@@ -10,5 +10,3 @@ def test_get_tasks(mock_db_session) -> None:
     response: Response = client.get("api/v1/tasks")
 
     assert response.status_code == 200
-    # data = response.json()
-    # assert data["name"] == ""
