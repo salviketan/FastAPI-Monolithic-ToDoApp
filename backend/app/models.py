62f11dd35eb630f7e6351696275d4ba4d9fd6fd6
salviketan <ketan.salvi777@gmail.com>
2026-06-20 21:03:01 +0530

    impl users model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, DateTime, Integer, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base_class import Base
 
@@ -12,7 +12,43 @@ from app.db.base_class import Base
 #     password: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
-class Task(Base):
+class Users(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(16), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(16), nullable=False)
+    username: Mapped[str] = mapped_column(String(32), nullable=False)
+    email: Mapped[str] = mapped_column(String(128), index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(256), nullable=True)
+    # profile_img: Mapped[str | None] = mapped_column(
+    #     String(256),
+    #     nullable=True,
+    #     default=None,
+    # )
+    active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+    tasks: Mapped[list["Tasks"]] = relationship(
+        back_populates="owner",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+    )
+
+    # @property
+    # def image_path(self) -> str:
+    #     if self.profile_img:
+    #         return f"/media/profile_pics/{self.profile_img}"
+    #     return "/static/profile_pics/default.jpg"
+
+
+class Tasks(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(250), index=True, nullable=False)
     completed: Mapped[bool] = mapped_column(Boolean, default=False)
@@ -26,3 +62,10 @@ class Task(Base):
         server_default=func.now(),
         onupdate=func.now(),
     )
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+
+    owner: Mapped["Users"] = relationship(back_populates="tasks")
