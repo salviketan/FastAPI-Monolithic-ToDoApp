dd40189dc6cd931dd0f6b49d4103529663b0f3b4
salviketan <ketan.salvi777@gmail.com>
2026-06-21 18:19:16 +0530

    added users api

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import tasks
+from app.api.api_v1.endpoints import tasks, users
 
 api_router = APIRouter()
 
 api_router.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
