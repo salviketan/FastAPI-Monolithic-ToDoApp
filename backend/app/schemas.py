de23c6f1a15190e3e763b95ee436610e456f1990
salviketan <ketan.salvi777@gmail.com>
2026-06-21 18:15:26 +0530

    impl user schema

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,6 +1,41 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+##### Task Table Schema #####
+# Shared properties
+class UserPublicBase(BaseModel):
+    first_name: str | None = Field(default=None, min_length=1, max_length=16)
+    last_name: str | None = Field(default=None, min_length=1, max_length=16)
+    username: str | None = Field(default=None, min_length=4, max_length=32)
+
+
+class UserPrivateBase(BaseModel):
+    first_name: str = Field(min_length=1, max_length=16)
+    last_name: str = Field(min_length=1, max_length=16)
+    email: EmailStr = Field(max_length=128)
+
+
+# Properties to receive on User creation
+class UserCreate(UserPrivateBase):
+    password: str = Field(min_length=8)
+
+
+# Properties to receive on User update
+class UserUpdate(UserPublicBase):
+    email: EmailStr | None = Field(default=None, max_length=128)
+    active: bool | None = Field(default=None)
+
+
+# Properties shared by models stored in DB
+class UserInDBBase(UserPublicBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+
+
+#############################
 
 
 ##### Task Table Schema #####
@@ -27,9 +62,21 @@ class TaskInDBBase(TaskBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    owner_id: int
+    owner: UserInDBBase
+
+
+#############################
 
 
 # ---------------------------------------------
+
+
+# Properties to return to client
+class User(UserInDBBase):
+    active: bool
+    created_at: datetime
+    updated_at: datetime
 
 
 # Properties to return to client
@@ -40,6 +87,11 @@ class Task(TaskInDBBase):
 
 
 # ---------------------------------------------
+
+
+# Properties stored in DB
+class UserInDB(UserInDBBase):
+    pass
 
 
 # Properties stored in DB
