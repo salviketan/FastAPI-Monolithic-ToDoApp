855ccc38031344c5395ca711a72ac46b46de9e3d
salviketan <ketan.salvi777@gmail.com>
2026-08-29 23:31:48 +0530

    async func

--- backend/app/api/api_v1/endpoints/tasks.py
+++ backend/app/api/api_v1/endpoints/tasks.py
@@ -2,7 +2,7 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud, models, schemas
 from app.api import deps
@@ -14,12 +14,12 @@ router = APIRouter()
     "",
     response_model=list[schemas.Task],
 )
-def get_tasks(
-    db: Annotated[Session, Depends(deps.get_db)],
+async def get_tasks(
+    db: Annotated[AsyncSession, Depends(deps.get_db)],
     skip: int = 0,
     limit: int = 100,
-) -> list[models.Tasks]:
-    tasks: list[models.Tasks] = crud.task.get_multi(
+) -> list[models.Task]:
+    tasks: list[models.Task] = await crud.task.get_multi(
         db,
         skip=skip,
         limit=limit,
@@ -33,21 +33,21 @@ def get_tasks(
     response_model=schemas.Task,
     status_code=status.HTTP_201_CREATED,
 )
-def create_task(
+async def create_task(
     *,
     task_in: schemas.TaskCreate,
-    db: Annotated[Session, Depends(deps.get_db)],
+    db: Annotated[AsyncSession, Depends(deps.get_db)],
 ) -> Any:
     task_in_data: dict[str, Any] = jsonable_encoder(task_in)
 
-    user: models.Users | None = crud.user.get(db=db, idx=task_in_data["owner_id"])
+    user: models.User | None = await crud.user.get(db=db, idx=task_in_data["owner_id"])
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User doesn't exists.",
         )
 
-    task: list[models.Tasks] | None = crud.task.get_by_kwargs(
+    task: list[models.Task] | None = await crud.task.get_by_kwargs(
         db=db,
         kwargs={"name": task_in_data["name"]},
     )
@@ -56,7 +56,7 @@ def create_task(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Task already exists.",
         )
-    task = crud.task.create(db, obj_in=task_in)
+    task = await crud.task.create(db, obj_in=task_in)
     return task
 
 
@@ -65,12 +65,12 @@ def create_task(
     response_model=schemas.Task,
     status_code=status.HTTP_200_OK,
 )
-def get_task(
+async def get_task(
     *,
     id: int,  # noqa: A002
-    db: Annotated[Session, Depends(deps.get_db)],
+    db: Annotated[AsyncSession, Depends(deps.get_db)],
 ) -> Any:
-    task: models.Tasks | None = crud.task.get(db=db, idx=id)
+    task: models.Task | None = await crud.task.get(db=db, idx=id)
     if not task:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -84,19 +84,19 @@ def get_task(
     response_model=schemas.Task,
     status_code=status.HTTP_200_OK,
 )
-def update_task(
+async def update_task(
     *,
     id: int,  # noqa: A002
     task_in: schemas.TaskUpdate,
-    db: Annotated[Session, Depends(deps.get_db)],
+    db: Annotated[AsyncSession, Depends(deps.get_db)],
 ) -> Any:
-    task: models.Tasks | None = crud.task.get(db=db, idx=id)
+    task: models.Task | None = await crud.task.get(db=db, idx=id)
     if not task:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Task doesn't exists.",
         )
-    task = crud.task.update(db=db, db_obj=task, obj_in=task_in)
+    task = await crud.task.update(db=db, db_obj=task, obj_in=task_in)
 
     return task
 
@@ -106,17 +106,17 @@ def update_task(
     response_model=schemas.Task,
     status_code=status.HTTP_200_OK,
 )
-def delete_task(
+async def delete_task(
     *,
     id: int,  # noqa: A002
-    db: Annotated[Session, Depends(deps.get_db)],
+    db: Annotated[AsyncSession, Depends(deps.get_db)],
 ) -> Any:
-    task: models.Tasks | None = crud.task.get(db=db, idx=id)
+    task: models.Task | None = await crud.task.get(db=db, idx=id)
     if not task:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Task doesn't exists.",
         )
-    task = crud.task.remove(db=db, idx=id)
+    task = await crud.task.remove(db=db, idx=id)
 
     return task
