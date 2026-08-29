0b1460fd252889ed717c147d52757023ad15000f
salviketan <ketan.salvi777@gmail.com>
2026-08-29 23:53:41 +0530

    del unwanted code

--- backend/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.crud import task  # noqa: F401
-from app.models import Tasks  # noqa: F401
-from app.schemas import (
-    Task,  # noqa: F401
-    TaskCreate,  # noqa: F401
-    TaskInDB,  # noqa: F401
-    TaskUpdate,  # noqa: F401
-)
